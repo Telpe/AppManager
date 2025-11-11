a9afaa3bd94848e62f8900ad9252618a4ae23f6b
Loki Møller Madsen <tommy.s.madsen@gmail.com>
2025-11-11 08:59:37 +0100

    added buggy controls, this is a restore point

---
+++ AppManager/Pages/ActionTriggerDialog.xaml.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Windows;
+using AppManager.Actions;
+using AppManager.Triggers;
+
+namespace AppManager.Pages
+{
+    public partial class ActionTriggerDialog : Window
+    {
+        public ActionModel ResultAction { get; private set; }
+        public TriggerModel ResultTrigger { get; private set; }
+        public bool IsActionMode { get; }
+
+        public ActionTriggerDialog(ActionModel action = null, TriggerModel trigger = null)
+        {
+            InitializeComponent();
+            
+            IsActionMode = action != null;
+            
+            if (IsActionMode)
+            {
+                ActionEditor.CurrentAction = action ?? new ActionModel();
+                ActionEditor.ActionSaved += OnActionSaved;
+                ActionEditor.ActionCancelled += OnCancelled;
+                Title = "Edit Action";
+            }
+            else
+            {
+                TriggerEditor.CurrentTrigger = trigger ?? new TriggerModel();
+                TriggerEditor.TriggerSaved += OnTriggerSaved;
+                TriggerEditor.TriggerCancelled += OnCancelled;
+                Title = "Edit Trigger";
+            }
+        }
+
+        private void OnActionSaved(object sender, ActionModel action)
+        {
+            ResultAction = action;
+            DialogResult = true;
+            Close();
+        }
+
+        private void OnTriggerSaved(object sender, TriggerModel trigger)
+        {
+            ResultTrigger = trigger;
+            DialogResult = true;
+            Close();
+        }
+
+        private void OnCancelled(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
